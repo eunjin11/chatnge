a78a9661df563e6ca860542aeb2d69c5465d6ba1
Eunjin Cho <whdmswls9207@gmail.com>
2025-04-09 16:46:29 +0900

    feat/#7 : 뒤로가기 svg 적용

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 interface HeaderProps {
   title: string;
@@ -16,8 +17,8 @@ function Header({ title }: HeaderProps) {
   return (
     <div className="fixed top-0 left-0 right-0 bg-white shadow-[0px_2px_2px_0px_rgba(204,204,204,0.25)] p-4">
       <div className="flex items-center justify-between">
-        <button className="text-[#5F6368]" onClick={handleGoBack}>
-          {`<`}
+        <button className="text-[#5F6368] h-6 w-6" onClick={handleGoBack}>
+          <Image src="/BackArrow.svg" alt="back" width={10} height={10} />
         </button>
         <h1 className="text-lg font-normal text-black">{title}</h1>
         <div className="w-8" />
