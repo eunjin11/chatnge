595694ae1148049c1a381c48ed96dd1fa798d756
Eunjin Cho <whdmswls9207@gmail.com>
2025-04-30 04:38:17 +0900

    feat/#21: 사용하지 않는 api 삭제

--- app/api/emotion.ts
+++ app/api/emotion.ts
@@ -29,7 +29,7 @@ export async function findEmotionRecordsInRange(
 export async function findEmotionRecordByDate(
   userEmail: string,
   date: Date
-): Promise<EmotionRecord | null> {
+): Promise<EmotionResponse | null> {
   const { start, end } = getStartAndEndOfDay(date);
   return prisma.emotionRecord.findFirst({
     where: {
@@ -49,19 +49,8 @@ export async function createEmotionRecord(
     const userEmail = await getUserEmail();
     const date = emotionData.date || new Date();
 
-    const startOfDay = new Date(date.setHours(0, 0, 0, 0));
-    const endOfDay = new Date(date.setHours(23, 59, 59, 999));
-
     // 기존 기록이 있는지 확인
-    const existingRecord = await prisma.emotionRecord.findFirst({
-      where: {
-        userEmail,
-        date: {
-          gte: startOfDay,
-          lte: endOfDay,
-        },
-      },
-    });
+    const existingRecord = await findEmotionRecordByDate(userEmail, date);
 
     if (existingRecord) {
       // 기록이 있으면 업데이트
@@ -110,18 +99,6 @@ export async function getAiSummary(emotionData: EmotionData): Promise<string> {
   } catch (error) {
     console.error("Error creating emotion record:", error);
     throw new Error("ai 요약을 생성하는 중 오류가 발생했습니다.");
-  }
-}
-
-export async function getEmotionRecordByDate(
-  date: Date
-): Promise<EmotionRecord | null> {
-  try {
-    const userEmail = await getUserEmail();
-    return await findEmotionRecordByDate(userEmail, date);
-  } catch (error) {
-    console.error("Error fetching emotion record:", error);
-    throw new Error("감정 기록을 불러오는 중 오류가 발생했습니다.");
   }
 }
 
