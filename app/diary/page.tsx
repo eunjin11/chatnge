76dd38f93521407d866cec636c46ae9f06043453
Eunjin Cho <whdmswls9207@gmail.com>
2025-05-31 00:19:26 +0900

    feat/#25: api 호출 실패 시 에러 핸들링

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -5,6 +5,10 @@ import DiaryClient from "./_component/DiaryClient";
 export default async function DiaryPage() {
   const { monthRange, monthData } = await getMonthlyEmotionSummary(new Date());
 
+  if (!monthData) {
+    return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
+  }
+
   return (
     <>
       <DiaryClient
