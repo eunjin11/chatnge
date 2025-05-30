beead9085360955c6a539d13032a2ee427dbba2f
Eunjin Cho <whdmswls9207@gmail.com>
2025-05-31 00:20:49 +0900

    feat/#25: 동적 라우트에 대한 활성 상태 감지 개선

--- components/BottomNavigation.tsx
+++ components/BottomNavigation.tsx
@@ -10,14 +10,14 @@ const BottomNavigation = () => {
     <div className="fixed bottom-0 left-0 right-0 flex justify-between items-center py-4 px-6 border-t bg-white border-gray-200 rounded-t-[20px] shadow-lg max-w-md mx-auto">
       <Link
         href="/diary"
-        className={`flex flex-col items-center cursor-pointer ${pathname === "/diary" ? "text-primary" : "text-gray-500"}`}
+        className={`flex flex-col items-center cursor-pointer ${pathname.startsWith("/diary") ? "text-primary" : "text-gray-500"}`}
       >
         <Activity size={20} />
         <span className="text-xs mt-1">기록</span>
       </Link>
       <Link
         href="/medicine"
-        className={`flex flex-col items-center ${pathname === "/medicine" ? "text-primary" : "text-gray-500"}`}
+        className={`flex flex-col items-center ${pathname.startsWith("/medicine") ? "text-primary" : "text-gray-500"}`}
       >
         <Pill size={20} />
         <span className="text-xs mt-1">약 알리미</span>
@@ -31,14 +31,14 @@ const BottomNavigation = () => {
       </Link>
       <Link
         href="/chat"
-        className={`flex flex-col items-center ${pathname === "/chat" ? "text-primary" : "text-gray-500"}`}
+        className={`flex flex-col items-center ${pathname.startsWith("/chat") ? "text-primary" : "text-gray-500"}`}
       >
         <MessageSquare size={20} />
         <span className="text-xs mt-1">CHAT</span>
       </Link>
       <Link
         href="/mypage"
-        className={`flex flex-col items-center ${pathname === "/mypage" ? "text-primary" : "text-gray-500"}`}
+        className={`flex flex-col items-center ${pathname.startsWith("/mypage") ? "text-primary" : "text-gray-500"}`}
       >
         <User size={20} />
         <span className="text-xs mt-1">MYPAGE</span>
